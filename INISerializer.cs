97dd63173e54b15da06952f7297878ce5c5348c4
asdf1899 <araid.anas99@gmail.com>
2017-06-21 14:41:26 +0200

    Add getPropertiesInfo function

--- INISerializer.cs
+++ INISerializer.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace INISerializer
 {
-    public class INISerializer
-    {
-		Object obj;
-		public INISerializer(){}
-		public string SerializeObject(Object obj)
+	public static class INISerializer
+	{
+		public static string SerializeObject(object obj)
 		{
-			this.obj = obj;
-			return (obj == null) ? null : doTask();
+			return (obj == null) ? null : buildIniString(getPropertiesInfo(obj));
 		}
-		private string doTask()
+		private static Dictionary<string, string> getPropertiesInfo(object obj)
 		{
-			
+			Dictionary<string, string> _propertyDict = new Dictionary<string, string>();
+			PropertyInfo[] properties = obj.GetType().GetProperties();
+			foreach (var prop in properties)
+			{
+				_propertyDict.Add(prop.Name, prop.GetValue(obj, null).ToString());
+			}
+
+			return _propertyDict;
 		}
-    }
+		private static string buildIniString(Dictionary<string, string> props)
+		{
+
+			return "";
+		}
+	}
 }
